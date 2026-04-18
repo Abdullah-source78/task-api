8e4ca73aa6f7d67bd3c4df69f20593a9a478810a
Peer Abdullah <Peer@ecube.com>
2026-04-18 19:26:33 +0500

    Added Modals and Curd Operations

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-tasks = []
+class Task(BaseModel):
+    id: int
+    title: str
+    completed: bool = False
+
+tasks: List[Task] = []
 
 @app.get("/")
 def home():
@@ -13,6 +20,22 @@ def get_tasks():
     return tasks
 
 @app.post("/tasks")
-def create_task(task: dict):
+def create_task(task: Task):
     tasks.append(task)
-    return {"message": "Task added", "tasks": tasks}
+    return {"message": "Task added", "task": task}
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: Task):
+    for i, t in enumerate(tasks):
+        if t.id == task_id:
+            tasks[i] = updated_task
+            return {"message": "Task updated"}
+    return {"error": "Task not found"}
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for i, t in enumerate(tasks):
+        if t.id == task_id:
+            tasks.pop(i)
+            return {"message": "Task deleted"}
+    return {"error": "Task not found"}
