b29dd6125b4ad2b64bbcee792d7bf081d2f7be3b
Peer Abdullah <Peer@ecube.com>
2026-04-25 15:13:53 +0500

    Added README.md

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://taskuser:1234@localhost/taskdb"
+DATABASE_URL = "postgresql://taskuser:1234@172.17.0.1/taskdb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
