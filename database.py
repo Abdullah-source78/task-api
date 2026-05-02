276a19fa0a0f633c2c0fca4f6afe2cd9f667abce
Peer Abdullah <Peer@ecube.com>
2026-05-03 03:43:49 +0500

    Added updated code and fixes

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:taskapi1234@task-db.cmhgui44wadz.us-east-1.rds.amazonaws.com:5432/taskdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
